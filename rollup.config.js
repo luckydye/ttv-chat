b980207e55da9fb71d35359cc5adadc32c307811
Tim Havlicek <tim.h4vlicek@gmail.com>
2021-10-24 00:30:17 +0200

    rollup cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,5 @@
 import typescript from '@rollup/plugin-typescript';
-import Package from './package.json';
-
-import webWorkerLoader from 'rollup-plugin-web-worker-loader';
 import node_resolve from '@rollup/plugin-node-resolve';
-import injectProcessEnv from 'rollup-plugin-inject-process-env';
-
-const mode = process.env.NODE_ENV ? 'production' : 'development';
-console.log('Mode:', mode, '\n');
-
-const envVars = {
-    '__ENV__': mode,
-    '__PACKAGE__': {
-        version: Package.version,
-        name: Package.name,
-        description: Package.description,
-    }
-}
 
 export default [
     {
@@ -26,9 +10,7 @@ export default [
         },
         plugins: [
             node_resolve(),
-            webWorkerLoader(),
             typescript(),
-            injectProcessEnv(envVars),
         ],
     }
 ];
